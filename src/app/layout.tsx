cdf673eee9a8f81c19ecb591c5bf4b1da1a33a04
first0887671679 <firstpinna@gmail.com>
2026-07-23 23:17:58 +0700

    fix(gtm): update GTM container ID to GTM-T3CWRXSQ in head script and noscript iframe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -195,7 +195,7 @@ export default async function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-WV4QJSQM');`}
+          })(window,document,'script','dataLayer','GTM-T3CWRXSQ');`}
         </Script>
         {/* Google tag (gtag.js) */}
         <Script
@@ -213,7 +213,7 @@ export default async function RootLayout({
         className={`${prompt.variable} ${sarabun.variable} antialiased overflow-x-hidden w-full flex flex-col min-h-screen`}
       >
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WV4QJSQM"
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3CWRXSQ"
             height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
         </noscript>
         <script
