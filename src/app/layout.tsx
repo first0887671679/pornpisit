bd347ac42ba228e8e4fc552ddf0cd63dd4638a18
first0887671679 <firstpinna@gmail.com>
2026-07-24 23:18:11 +0700

    chore: ติดตั้ง Google Tag Manager GTM-TCPJKDTJ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -189,6 +189,17 @@ export default async function RootLayout({
         <link rel="dns-prefetch" href="https://images.unsplash.com" />
         <link rel="dns-prefetch" href="https://i.pravatar.cc" />
         <link rel="dns-prefetch" href="https://res.cloudinary.com" />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TCPJKDTJ');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Google tag (gtag.js) */}
         <script
           async
@@ -206,6 +217,16 @@ gtag('config', 'GT-PZVK64WN');`,
       <body
         className={`${prompt.variable} ${sarabun.variable} antialiased overflow-x-hidden w-full flex flex-col min-h-screen`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TCPJKDTJ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessJsonLd) }}
